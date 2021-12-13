1a911af00ab698081559d0093cbdd9987ea1c25d
Sam <mkr.samvel.99@gmail.com>
2021-12-13 19:24:18 +0200

    mainpage up and sword anim

--- js/atira.js
+++ js/atira.js
@@ -1,3 +1,12 @@
+function ibg(){
+    $.each($('.ibg'), function (index, val) {
+        if ($(this).find('img').length > 0) {
+            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+        }
+    });
+} 
+ibg();
+
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $('.header__burger,.header__menu').toggleClass('active');
@@ -11,11 +20,36 @@ $(document).ready(function () {
     });
 });
 
-function ibg(){
-    $.each($('.ibg'), function (index, val) {
-        if ($(this).find('img').length > 0) {
-            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                animItem.classList.remove('_active');
+            }
         }
-    });
-} 
-ibg();
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+}
