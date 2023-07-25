5bc78ecb0a67d574d806984c4991c326555daf1e
Sam <mkr.samvel.99@gmail.com>
2023-07-25 10:51:39 +0300

    25.07.2023
    25.07.2023

--- js/atira.js
+++ js/atira.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function ibg(){
     $.each($('.ibg'), function (index, val) {
         if ($(this).find('img').length > 0) {
@@ -98,75 +97,4 @@ $(document).ready(function () {
         waitForAnimate: true,
         centerMode: false,
     });
-=======
-function ibg(){
-    $.each($('.ibg'), function (index, val) {
-        if ($(this).find('img').length > 0) {
-            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-        }
-    });
-} 
-ibg();
-
-$(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
-
-$(document).ready(function () {
-    $('.cover-image').click(function (event) {
-        $('.cover-image').toggleClass('active')
-    });
-});
-
-
-
-const animItems = document.querySelectorAll('._anim-items');
-
-if (animItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll);
-    function animOnScroll() {
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
-
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_active');
-            } else {
-                animItem.classList.remove('_active');
-            }
-        }
-    }
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
-    }
-}
-
-$(document).ready(function () {
-    $('.slider__body').slick({
-        dots: true,
-        arrows: false,
-        accessibility: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplaySpeed: 3000,
-        adaptiveHeight: false,
-        speed: 800,
-        waitForAnimate: true,
-        centerMode: false,
-    });
->>>>>>> 84b08a70dd2864d275763c5202b4fd4b589537a6
 });
